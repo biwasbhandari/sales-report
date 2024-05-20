7bb3cec02f1c95ccb4886f25660ce79200f0300a
xbiwas <biwas2059@gmail.com>
2024-05-20 14:30:09 +0545

    customer model updated

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=120)
+    logo = models.ImageField(upload_to='customer', default='img.jpg')
+    
+    def __str__(self):
+        return str(self.name)
+    
+
